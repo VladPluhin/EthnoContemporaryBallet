158350d35da51ed3becdbccbf35ad1760309c577
VladislavPluhin <vlad.plugin@ukad-group.com>
2023-02-18 16:44:37 +0200

    WORK

--- src/components/sorts/sorts.js
+++ src/components/sorts/sorts.js
@@ -16,22 +16,5 @@ function getsortedCards(data, name) {
   return personDatas
 } 
 
-// function getSortedNav(data) { 
-
-//   console.log(data)
-//   let navData = []
-
-//   data.forEach(element => {
-//      if(element.textUrl) {
-//         element.sectionBlocks.map((link)=> {
-//           if(link.slug) {
-//             navData=[...navData, link.textUrl={id: link.id,  textNavLink: link.nameEvent ? link.nameEvent : link.namePersone, linkSludg: link.slug }]
-//           }
-//        })
-//        navData=[...navData, element.textUrl= {  id: element.id, textNavLink: element.textNavigationLink, linkSludg: element.textUrl}]
-//       }
-//   });
-// }
-
 
 export {getsortedData, getsortedCards }
